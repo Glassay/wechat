335547dc3dca5f5e2c8570dcfbef9acfadc4825b
Glassay <31647920+Glassay@users.noreply.github.com>
2017-10-17 20:17:56 +0800

    小程序

--- blog/dist/pages/hot.js
+++ blog/dist/pages/hot.js
@@ -31,12 +31,7 @@ var Blog = function (_wepy$page) {
       args[_key] = arguments[_key];
     }
 
-    return _ret = (_temp = (_this = _possibleConstructorReturn(this, (_ref = Blog.__proto__ || Object.getPrototypeOf(Blog)).call.apply(_ref, [this].concat(args))), _this), _this.config = {}, _this.data = {
-      pic1: '../image/home.png',
-      pic2: '../image/message.png',
-      pic3: '../image/like.png',
-      pic4: '../image/github.png'
-    }, _this.methods = {}, _temp), _possibleConstructorReturn(_this, _ret);
+    return _ret = (_temp = (_this = _possibleConstructorReturn(this, (_ref = Blog.__proto__ || Object.getPrototypeOf(Blog)).call.apply(_ref, [this].concat(args))), _this), _this.config = {}, _this.data = {}, _this.methods = {}, _temp), _possibleConstructorReturn(_this, _ret);
   }
 
   return Blog;
@@ -45,4 +40,4 @@ var Blog = function (_wepy$page) {
 
 Page(require('./../npm/wepy/lib/wepy.js').default.$createPage(Blog , 'pages/hot'));
 
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImhvdC5qcyJdLCJuYW1lcyI6WyJCbG9nIiwiY29uZmlnIiwiZGF0YSIsInBpYzEiLCJwaWMyIiwicGljMyIsInBpYzQiLCJtZXRob2RzIiwicGFnZSJdLCJtYXBwaW5ncyI6Ijs7Ozs7OztBQUNFOzs7Ozs7Ozs7Ozs7SUFDcUJBLEk7Ozs7Ozs7Ozs7Ozs7O2tMQUNuQkMsTSxHQUFTLEUsUUFFVEMsSSxHQUFPO0FBQ0xDLFlBQU0sbUJBREQ7QUFFTEMsWUFBTSxzQkFGRDtBQUdMQyxZQUFNLG1CQUhEO0FBSUxDLFlBQU07QUFKRCxLLFFBTVBDLE8sR0FBVSxFOzs7O0VBVHNCLGVBQUtDLEk7O2tCQUFsQlIsSSIsImZpbGUiOiJob3QuanMiLCJzb3VyY2VzQ29udGVudCI6WyJcbiAgaW1wb3J0IHdlcHkgZnJvbSAnd2VweSdcbiAgZXhwb3J0IGRlZmF1bHQgY2xhc3MgQmxvZyBleHRlbmRzIHdlcHkucGFnZSB7XG4gICAgY29uZmlnID0ge1xuICAgIH1cbiAgICBkYXRhID0ge1xuICAgICAgcGljMTogJy4uL2ltYWdlL2hvbWUucG5nJyxcbiAgICAgIHBpYzI6ICcuLi9pbWFnZS9tZXNzYWdlLnBuZycsXG4gICAgICBwaWMzOiAnLi4vaW1hZ2UvbGlrZS5wbmcnLFxuICAgICAgcGljNDogJy4uL2ltYWdlL2dpdGh1Yi5wbmcnXG4gICAgfVxuICAgIG1ldGhvZHMgPSB7XG4gICAgfVxuICB9XG4iXX0=
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImhvdC5qcyJdLCJuYW1lcyI6WyJCbG9nIiwiY29uZmlnIiwiZGF0YSIsIm1ldGhvZHMiLCJwYWdlIl0sIm1hcHBpbmdzIjoiOzs7Ozs7O0FBQ0U7Ozs7Ozs7Ozs7OztJQUNxQkEsSTs7Ozs7Ozs7Ozs7Ozs7a0xBQ25CQyxNLEdBQVMsRSxRQUVUQyxJLEdBQU8sRSxRQUVQQyxPLEdBQVUsRTs7OztFQUxzQixlQUFLQyxJOztrQkFBbEJKLEkiLCJmaWxlIjoiaG90LmpzIiwic291cmNlc0NvbnRlbnQiOlsiXG4gIGltcG9ydCB3ZXB5IGZyb20gJ3dlcHknXG4gIGV4cG9ydCBkZWZhdWx0IGNsYXNzIEJsb2cgZXh0ZW5kcyB3ZXB5LnBhZ2Uge1xuICAgIGNvbmZpZyA9IHtcbiAgICB9XG4gICAgZGF0YSA9IHtcbiAgICB9XG4gICAgbWV0aG9kcyA9IHtcbiAgICB9XG4gIH1cbiJdfQ==
